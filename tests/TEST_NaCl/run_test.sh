661943e65f182eb80126c6ce44228fd4f2505a13
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2015-02-03 20:50:25 +0100

    Testing: Use all arguments for calling executable. (This allows to pass mpirun)

--- tests/TEST_NaCl/run_test.sh
+++ tests/TEST_NaCl/run_test.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
-if [ -z "$1" ]; then
+if [ -z "$@" ]; then
 	echo "Syntax: run_test.sh <LAMMPS-executable>"
 	exit 999
 fi
 
 for a0 in 3.0 4.0 5.0 7.0 10.0; do
 
-  cat lammps.in | sed "s/%%a0/${a0}/g" | $1 > OUT.$a0
+  cat lammps.in | sed "s/%%a0/${a0}/g" | $@ > OUT.$a0
   mv log.lammps log.lammps.$a0
   mv dump.custom dump.custom.$a0
 
