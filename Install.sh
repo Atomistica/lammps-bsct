6d74eb39e82eba3338a9f69fc4ef9651b5c9cb1f
jotelha <johannes.hoermann@imtek.uni-freiburg.de>
2019-10-11 16:34:01 +0200

    Install.sh simplified

--- Install.sh
+++ Install.sh
@@ -1,14 +1,23 @@
-# Install/unInstall package files in LAMMPS
+## Install/unInstall package files in LAMMPS
+
+package_files="
+  anderson_mixer.h anderson_mixer.cpp
+  fix_bsct.h fix_bsct.cpp fix_bsct_parameters_t.h
+  pair_coul_cut_bsct.h pair_coul_cut_bsct.cpp
+  pair_lj_charmmfsw_coul_charmmfsh_bsct.h pair_lj_charmmfsw_coul_charmmfsh_bsct.cpp
+  pair_coul_long_bsct.h pair_coul_long_bsct.cpp
+  pair_lj_charmmfsw_coul_long_bsct.h pair_lj_charmmfsw_coul_long_bsct.cpp
+  pppm_bsct.h pppm_bsct.cpp"
 
 if (test $1 = 2) then
 
-    for i in anderson_mixer.h anderson_mixer.cpp fix_bsct.h fix_bsct.cpp fix_bsct_parameters_t.h pair_coul_cut_bsct.h pair_coul_cut_bsct.cpp pair_coul_long_bsct.h pair_coul_long_bsct.cpp pppm_bsct.h pppm_bsct.cpp ; do
+    for i in $package_files; do
         diff -q ../$i $i
     done
 
 elif (test $1 = 1) then
 
-    for i in anderson_mixer.h anderson_mixer.cpp fix_bsct.h fix_bsct.cpp fix_bsct_parameters_t.h pair_coul_cut_bsct.h pair_coul_cut_bsct.cpp pair_coul_long_bsct.h pair_coul_long_bsct.cpp pppm_bsct.h pppm_bsct.cpp ; do
+    for i in $package_files; do
 	cp -p $i ..
     done
 
@@ -20,7 +29,7 @@ elif (test $1 = 1) then
 
 elif (test $1 = 0) then
 
-    for i in anderson_mixer.h anderson_mixer.cpp fix_bsct.h fix_bsct.cpp fix_bsct_parameters_t.h pair_coul_cut_bsct.h pair_coul_cut_bsct.cpp pair_coul_long_bsct.h pair_coul_long_bsct.cpp pppm_bsct.h pppm_bsct.cpp ; do
+    for i in $package_files; do
 	rm -f ../$i
     done
 
