d48f5cecba9b3a77dda553e500e6c8c107010c3e
cs137@uni-freiburg.de <fr_cs137@n4667.nemo.privat>
2019-10-24 13:31:57 +0200

    latest updates

---
+++ tests/TEST_NaCl_charmmfsw_coul_long_bsct/eval.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+
+from StringIO import StringIO
+
+import numpy as np
+
+###
+
+def loadstate(logfn, dumpfn):
+    f = open(logfn)
+    l = f.readline()
+    while len(l.split()) == 0 or l.split()[0] != 'Step':
+        l = f.readline()
+    l = f.readline().split()
+    ect, ecoul, elong, lx = map(float, [l[4], l[6], l[5], l[8]])
+    f.close()
+
+    types, charges = np.loadtxt(dumpfn, skiprows=9, usecols=[1, 5], unpack=True)
+
+    return lx, ect, ecoul, elong, types, charges
+
+###
+
+M = 1.74756459463318219 # Madelung constant of Na-Cl
+nat = 64                # Number of atoms
+
+X = 1.084
+U = 5.0
+V = 5.0
+p = 2
+
+verbose = False
+
+###
+
+for a0 in [3.0, 4.0, 5.0, 7.0, 10.0]:
+    if verbose:
+        print '=== log.lammps.{}, dump.custom.{} ==='.format(a0, a0)
+
+    lx, ect, ecoul, elong, types, charges = loadstate('log.lammps.{}'.format(a0), 'dump.custom.{}'.format(a0))
+    charges1 = charges[types==1]
+    charges2 = charges[types==2]
+
+    # Charges should have equal magnitude but opposite sign
+
+    assert np.all(np.abs(charges1+charges2) < 1e-6)
+
+    # Check Coulomb energy
+
+    r0 = lx/2 # Lattice constant
+    charge = np.mean(charges1-charges2)/2
+    ecoul_check = -charge**2/r0 * M * nat
+
+    if verbose:
+        print 'Coulomb energy error:', (ecoul+elong-ecoul_check)/ecoul_check
+
+    assert abs(ecoul+elong-ecoul_check) < 1e-5
+
+    # Check energy from charge-transfer model
+
+    ect_check  =  -X*charge
+    ect_check += 0.5*U*charge**2
+    ect_check += 0.5*V*charge**p
+    ect_check *= nat
+
+    if verbose:
+        print 'CT energy error:', (ect-ect_check)/ect_check
+
+    assert abs(ect-ect_check) < 1e-6
+
+    # Check charges
+    # Total energy:    -q**2*M/r0 - X*q + 0.5*(U+V)*q**2
+    # Derivative:      -2*q*M/r0 - X + (U+V)*q
+
+    # Equilibrium at
+    charge_check = X/(U+V-2*M/r0)
+
+    if verbose:
+        print 'Charge error:', abs(charge-charge_check)/charge_check
+    assert abs(charge-charge_check) < 1e-6
---
+++ tests/TEST_NaCl_charmmfsw_coul_long_bsct/run_test.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+if [ -z "$1" ]; then
+	echo "Syntax: run_test.sh <LAMMPS-executable>"
+	exit 999
+fi
+
+for a0 in 3.0 4.0 5.0 7.0 10.0; do
+
+  cat lammps.in | sed "s/%%a0/${a0}/g" | $@ > OUT.$a0
+  mv log.lammps log.lammps.$a0
+  mv dump.custom dump.custom.$a0
+
+done
